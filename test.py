ca7be27c9c0103a376ad90190b8e2bddfb3fd8ee
FlorianHaschke <80045771+FlorianHaschke@users.noreply.github.com>
2023-11-14 22:12:25 +0100

    🐛 wlan and mobilephone

--- test.py
+++ test.py
@@ -5,6 +5,18 @@ import socket
 import time
 import math
 
+sta_if = network.WLAN(network.STA_IF)
+sta_if.active(True)
+sta_if.connect("WLAN-197399", "32734056271867621744")
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('', 8081))
+s.listen(5)
+
+sta_if = network.WLAN(network.STA_IF)
+print(sta_if.ifconfig()[0])
+print("WLAN-Status:", sta_if.isconnected())
+
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
 
@@ -20,14 +32,14 @@ total_count = 0  # Anzahl der bisherigen Werte
 
 
 UREF = 3.3
-NULLPUNKT = 2.820 
+NULLPUNKT = 2.5
 VpA = 0.185
 
 
 
 def start_server():
     addr = wlan.ifconfig()[0]
-    port = 82
+    port = 89
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((addr, port))
@@ -38,7 +50,7 @@ def start_server():
     while True:
         led.on()
         sensor_value = sensor_pin.read_u16()  # Lese den analogen Wert
-        window_size = 20  # Größe des gleitenden Mittelwerts-Fensters
+        window_size = 1  # Größe des gleitenden Mittelwerts-Fensters
         sensor_values = [0] * window_size  # Eine Liste, um die letzten Werte zu speichern
         index = 0  # Index, um die Position des aktuellen Werts im Array zu verfolgen
         total_sum = 0  # Summe aller bisherigen Werte
@@ -60,10 +72,22 @@ def start_server():
         print('Connection from {}'.format(addr))
         request = conn.recv(1024)
         print('Request: {}'.format(request))
-        html_content = '<!DOCTYPE html>\n<html><body style="background: linear-gradient(to bottom, white, yellow);text-align:center;vertical-align:middle;line-height:100vh;"><div style="display:inline-block;font-size:24px;color:black;">Die aktuelle Ladeleistung betraegt {} Watt</div></body></html>'.format(chargingPower)
-        response = 'HTTP/1.0 200 OK\r\n\r\n{}'.format(html_content)
-        # response = 'HTTP/1.0 200 OK\r\n\r\n<!DOCTYPE html>\n<html><body style="background-color:white">{}</body></html>'.format((overall_average/65535)*UREF)
+       
+        html_content = """<!DOCTYPE html>
+        <html>
+        <head>
+            <style>
+                @import url('https://fonts.googleapis.com/css2?family=Comic+Sans+MS&display=swap');
+            </style>
+        </head>
+        <body style="background-color:white; background-image:linear-gradient(to bottom, white, yellow); text-align:center; vertical-align:middle; line-height:100vh; margin:0; font-family:'Comic Sans MS',cursive;">
+            <div style="display:flex; align-items:center; justify-content:center; font-size:5vw; color:black;">
+                Die aktuelle Ladeleistung betraegt {:.2f} Watt
+            </div>
+        </body>
+        </html>""".format((overall_average/65535)*UREF)
 
+        response = 'HTTP/1.0 200 OK\r\n\r\n{}'.format(html_content)
         # print("Aktueller Analogwert:", sensor_value)
         # print("Gleitender Mittelwert der letzten 10 Werte:", (moving_average/65535)*UREF)
         # print("Gleitender Amperewert der letzten 10 Werte:", (((moving_average/65535)*UREF) - NULLPUNKT)/VpA)
